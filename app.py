35e5cbab35edc8389f53cf6c8569159f81bf1f5d
David <davidabelman@gmail.com>
2014-06-05 11:56:16 +0100

    Added manage and models.py, as per http://www.realpython.com/blog/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/#.U5BED5RdVYg

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
 print "ENVIRNOMENT IS %s" %(os.environ['APP_SETTINGS'])
+
+from models import Result
 
 @app.route('/')
 def hello():
